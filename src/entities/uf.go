e09248b8aa3c1f8681f3fa003c2456832d805e0c
gustavo <duarte.guga2025@gmail.com>
2024-07-11 16:03:05 -0400

    Definido valores retornados para UF

--- src/entities/uf.go
+++ src/entities/uf.go
@@ -1,8 +1,7 @@
 package entities
 
 type UF struct {
-	UF             string `json:"uf"`
-	Count          int    `json:"count"`
-	Total_Death    int    `json:"total_death"`
-	Total_Involved int    `json:"total_involved"`
+	Count          int `json:"count"`
+	Total_Death    int `json:"total_death"`
+	Total_Involved int `json:"total_involved"`
 }
