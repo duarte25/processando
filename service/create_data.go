3a26608e13534a12bf1f6cb647f430f71e6c9ed7
gustavo <duarte.guga2025@gmail.com>
2024-07-14 14:27:53 -0400

    Separando o código de criar uf

---
+++ service/create_data.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"processando/acidente"
+	"processando/src/configs"
+)
+
+type YearData struct {
+	UFData map[string]acidente.UFData `json:"uf_data"`
+}
+
+func CreateData() {
+	// Carregar as configurações
+	err := configs.Load()
+	if err != nil {
+		log.Fatalf("Erro ao carregar configurações: %v", err)
+	}
+
+	ctx := context.Background()
+
+	// Obter o cliente Redis do pacote configs
+	rdb := configs.GetRedisClient()
+	defer rdb.Close()
+
+	// Chama a função para processar os acidentes
+	result2021 := acidente.Acidente("./Acidentes_DadosAbertos_20230412.csv", "uf_acidente", "data_acidente", "2021")
+	result2022 := acidente.Acidente("./Acidentes_DadosAbertos_20230412.csv", "uf_acidente", "data_acidente", "2022")
+
+	// Criar um mapa para armazenar os dados por ano
+	result := make(map[string]YearData)
+
+	// Adicionar os resultados de 2021 e 2022 ao mapa result
+	result["2021"] = YearData{UFData: result2021}
+	result["2022"] = YearData{UFData: result2022}
+
+	// Itera sobre os dados e insere no Redis
+	for year, yearData := range result {
+		redisKey := fmt.Sprintf("dados_acidentes_%s", year)
+
+		// Iterar sobre os dados de cada estado e inseri-los como hash
+		for state, ufData := range yearData.UFData {
+			data, err := json.Marshal(ufData)
+			if err != nil {
+				log.Fatalf("Erro ao converter dados para JSON: %v", err)
+			}
+
+			err = rdb.HSet(ctx, redisKey, state, data).Err()
+			if err != nil {
+				log.Fatalf("Erro ao inserir dados no Redis: %v", err)
+			}
+		}
+	}
+
+	fmt.Println("Dados inseridos com sucesso!")
+}
--- src/middleware/cors.go
+++ src/middleware/cors.go
@@ -1,20 +1,19 @@
+// middleware/cors.go
 package middleware
 
 import (
 	"net/http"
+
+	"github.com/go-chi/cors"
 )
 
 func CORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
+	return cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // Permita apenas o frontend local
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Cache pré-checada por 5 minutos
+	})(next)
 }
